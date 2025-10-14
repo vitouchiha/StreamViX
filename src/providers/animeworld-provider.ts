2b59b7fb8cd36245ecbbefd81b87ec8e2d4c8d88
qwertyuiop8899 <emanuele.schiano+vix@gmail.com>
2025-10-14 16:00:19 +0200

    Update animeworld-provider.ts

--- src/providers/animeworld-provider.ts
+++ src/providers/animeworld-provider.ts
@@ -167,7 +167,7 @@ const exactMap: Record<string, string> = {
 
 
       "Ranma \u00bd (2024) Season 2": "Ranma \u00bd (2024) 2",
-
+      "Ranma1/2 (2024) Season 2": "Ranma \u00bd (2024) 2",
 
 
   // << AUTO-INSERT-EXACT >> (non rimuovere questo commento)
