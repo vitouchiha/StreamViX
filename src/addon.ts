287cb662c0ab96ad496ccdfe102191fe75f73f90
qwertyuiop8899 <emanuele.schiano+vix@gmail.com>
2025-12-05 10:17:45 +0100

    fix: freeshot

--- src/addon.ts
+++ src/addon.ts
@@ -10,7 +10,7 @@ import { AnimeWorldProvider } from './providers/animeworld-provider';
 import { KitsuProvider } from './providers/kitsu';
 import { formatMediaFlowUrl } from './utils/mediaflow';
 import { mergeDynamic, loadDynamicChannels, purgeOldDynamicEvents, invalidateDynamicChannels, getDynamicFilePath, getDynamicFileStats } from './utils/dynamicChannels';
-// --- Lightweight declarations to avoid TS complaints if @types/node non installati ---
+// --- Lightweight declarations to avoid TS complaints if @types/node non installati --- 
 // (Non sostituiscono l'uso consigliato di @types/node, ma evitano errori bloccanti.)
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const __dirname: string;
