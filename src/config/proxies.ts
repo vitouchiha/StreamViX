6b868b9a6c277cd48b37caf10b2a3c239014c186
qwertyuiop8899 <emanuele.schiano+vix@gmail.com>
2026-01-16 13:52:24 +0100

    fix: proxy

--- src/config/proxies.ts
+++ src/config/proxies.ts
@@ -5,20 +5,6 @@
 
 export const PROXIES: string[] = [
   // Example entries (replace with real ones)
-  'http://emaschipx-rotate:emaschipx@p.webshare.io:80/',
-  'http://proxooo4-rotate:proxooo4@p.webshare.io:80/',
-  'http://fabiorealdebrid-rotate:MammamiaHF1@p.webshare.io:80/',
-  'http://proxoooo-rotate:proxoooo@p.webshare.io:80/',
-  'http://teststremio-rotate:teststremio@p.webshare.io:80/',
-  'http://mammapro-rotate:mammapro@p.webshare.io:80/',
-  'http://iuhcxjzk-rotate:b3oqk3q40awp@p.webshare.io:80/',
-  'http://zmjoluhu-rotate:ej6ddw3ily90@p.webshare.io:80/',
-  'http://kkuafwyh-rotate:kl6esmu21js3@p.webshare.io:80/',
-  'http://stzaxffz-rotate:ax92ravj1pmm@p.webshare.io:80/',
-  'http://nfokjhhu-rotate:ez248bgee4z9@p.webshare.io:80/',
-  'http://fiupzkjx-rotate:0zlrd2in3mrh@p.webshare.io:80/',
-  'http://tpnvndgp-rotate:xjp0ux1wwc7n@p.webshare.io:80/',
-  'http://tmglotxc-rotate:stlrhx17nhqj@p.webshare.io:80/',
 ];
 
 let rr = 0;
